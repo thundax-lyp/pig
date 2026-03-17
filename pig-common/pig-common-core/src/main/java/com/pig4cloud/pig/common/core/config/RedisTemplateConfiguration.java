6e2100b6cf1043e2199d999b87b9d470d539df35
thundax-lyp <731378491@qq.com>
2026-03-17 15:02:20 +0800

    refactor

--- pig-common/pig-common-core/src/main/java/com/pig4cloud/pig/common/core/config/RedisTemplateConfiguration.java
+++ pig-common/pig-common-core/src/main/java/com/pig4cloud/pig/common/core/config/RedisTemplateConfiguration.java
@@ -18,24 +18,15 @@ package com.pig4cloud.pig.common.core.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
-import com.pig4cloud.pig.common.core.constant.CacheConstants;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
-import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.*;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.RedisSerializer;
-
-import java.time.Duration;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Redis 配置类
@@ -43,7 +34,6 @@ import java.util.Map;
  * @author lengleng
  * @date 2025/05/30
  */
-@EnableCaching
 @AutoConfiguration
 @AutoConfigureBefore(RedisAutoConfiguration.class)
 public class RedisTemplateConfiguration {
@@ -65,38 +55,6 @@ public class RedisTemplateConfiguration {
 		redisTemplate.setHashValueSerializer(jsonSerializer);
 		redisTemplate.setConnectionFactory(factory);
 		return redisTemplate;
-	}
-
-	/**
-	 * 自定义Spring Cache的Redis配置，统一使用Jackson序列化并按缓存名设置TTL
-	 * @param objectMapper Jackson 对象映射器
-	 * @return RedisCacheManager 构建器自定义器
-	 */
-	@Bean
-	public RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer(ObjectMapper objectMapper) {
-		RedisSerializer<Object> jsonSerializer = redisSerializer(objectMapper);
-		RedisSerializationContext.SerializationPair<Object> serializationPair = RedisSerializationContext.SerializationPair
-			.fromSerializer(jsonSerializer);
-
-		RedisCacheConfiguration defaultCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
-			.serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(RedisSerializer.string()))
-			.serializeValuesWith(serializationPair)
-			.disableCachingNullValues()
-			.entryTtl(Duration.ofMinutes(30));
-
-		Map<String, RedisCacheConfiguration> cacheConfigurations = new HashMap<>();
-		cacheConfigurations.put(CacheConstants.DEFAULT_CODE_CACHE,
-				defaultCacheConfiguration.entryTtl(Duration.ofSeconds(60)));
-		cacheConfigurations.put(CacheConstants.USER_DETAILS, defaultCacheConfiguration.entryTtl(Duration.ofMinutes(30)));
-		cacheConfigurations.put(CacheConstants.MENU_DETAILS, defaultCacheConfiguration.entryTtl(Duration.ofMinutes(30)));
-		cacheConfigurations.put(CacheConstants.ROLE_DETAILS, defaultCacheConfiguration.entryTtl(Duration.ofMinutes(30)));
-		cacheConfigurations.put(CacheConstants.DICT_DETAILS, defaultCacheConfiguration.entryTtl(Duration.ofHours(1)));
-		cacheConfigurations.put(CacheConstants.PARAMS_DETAILS, defaultCacheConfiguration.entryTtl(Duration.ofHours(1)));
-		cacheConfigurations.put(CacheConstants.CLIENT_DETAILS_KEY,
-				defaultCacheConfiguration.entryTtl(Duration.ofHours(12)));
-
-		return builder -> builder.cacheDefaults(defaultCacheConfiguration)
-			.withInitialCacheConfigurations(cacheConfigurations);
 	}
 
 	private RedisSerializer<Object> redisSerializer(ObjectMapper objectMapper) {
