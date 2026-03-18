8045ae1b84395e4715b7254eca6000400b45ffdd
thundax-lyp <731378491@qq.com>
2026-03-18 10:30:48 +0800

    refactor

---
+++ pig-upms/pig-upms-contract/src/main/java/com/pig4cloud/pig/sys/api/vo/RoleVO.java
@@ -0,0 +1,43 @@
+/*
+ *    Copyright (c) 2018-2025, lengleng All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * Neither the name of the pig4cloud.com developer nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * Author: lengleng (wangiegie@gmail.com)
+ */
+
+package com.pig4cloud.pig.sys.api.vo;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+/**
+ * 角色展示对象
+ *
+ * @author lengleng
+ * @date 2020/2/10
+ */
+@Data
+@Schema(description = "角色展示对象")
+public class RoleVO {
+
+	/**
+	 * 角色ID
+	 */
+	private Long roleId;
+
+	/**
+	 * 菜单列表
+	 */
+	private String menuIds;
+
+}
