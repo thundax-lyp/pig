7d4598554003cabee27bc929ba9d8a275973ace8
thundax-lyp <731378491@qq.com>
2026-03-19 10:43:55 +0800

    refactor

--- bacon-common/bacon-common-xss/src/main/java/com/github/thundax/bacon/common/xss/core/XssCleanInterceptor.java
+++ bacon-common/bacon-common-xss/src/main/java/com/github/thundax/bacon/common/xss/core/XssCleanInterceptor.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (c) 2019-2029, Dreamlu 卢春梦 (596392912@qq.com & www.dreamlu.net).
- * <p>
- * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE 3.0;
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p>
- * http://www.gnu.org/licenses/lgpl.html
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.github.thundax.bacon.common.xss.core;
 
 import cn.hutool.core.util.ArrayUtil;
@@ -21,6 +5,7 @@ import com.github.thundax.bacon.common.xss.config.BaconXssProperties;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NonNull;
 import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.AsyncHandlerInterceptor;
@@ -46,7 +31,7 @@ public class XssCleanInterceptor implements AsyncHandlerInterceptor {
 	 * @throws Exception 处理过程中可能抛出的异常
 	 */
 	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+	public boolean preHandle(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull Object handler)
 			throws Exception {
 		// 1. 非控制器请求直接跳出
 		if (!(handler instanceof HandlerMethod)) {
--- bacon-common/bacon-common-xss/src/main/java/com/github/thundax/bacon/common/xss/package-info.java
+++ bacon-common/bacon-common-xss/src/main/java/com/github/thundax/bacon/common/xss/package-info.java
@@ -1,4 +1,1 @@
-/**
- * 此包代码来源至: https://gitee.com/596392912/mica/tree/master/mica-xss
- */
 package com.github.thundax.bacon.common.xss;
